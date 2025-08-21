ac87d7a42cee8005ca958d059a68466c8505ed1c
efekurnaz <efekurnaz@gmail.com>
2025-08-21 12:00:16 +0300

    :hammer: fix: some

--- nvim/lua/core/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Search and replace keymaps
-vim.keymap.set('n', '<leader>S', ':%s/\\v//gc<left><left><left><left>', { desc = 'Search and replace in file' })
-vim.keymap.set('v', '<leader>s', ':s/\\v//gc<left><left><left><left>', { desc = 'Search and replace in selection' })
-
--- Use telescope for better search experience
-vim.keymap.set('n', '<leader>fs', require('telescope.builtin').live_grep, { desc = 'Search text in workspace' })
-vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = 'Search current word' })
